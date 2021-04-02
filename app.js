6816f6196b83b89000294ff327af24dbb754cd57
Sobirbek Sobirov <ssobirov3@students.wiut.uz>
2021-04-02 15:04:36 +0500

    modifications on styles of home page

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+
+app.set('view engine', 'pug');
+
+app.use('/static', express.static('public'));
+
+// localhost:8000
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+
+app.listen(8000, (err) => {
+    if (err) console.log(err);
+
+    console.log('Server is running on the port 8000');
+})
