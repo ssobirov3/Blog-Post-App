901bc29b4eda4e667444b226f6fcab3fcfc6d630
Sobirbek Sobirov <ssobirov3@students.wiut.uz>
2021-04-07 06:13:28 +0500

    Archive button is added styles and enabled

--- app.js
+++ app.js
@@ -25,9 +25,11 @@ app.get('/create', (req, res) => {
 
 app.post('/create', (req, res) => {
     const title = req.body.title
+    const snippet = req.body.snippet
     const description = req.body.description
 
-    if (title.trim() === '' && description.trim() === '') {
+
+    if (title.trim() === '' && description.trim() === '' && snippet.trim() === '') {
         res.render('create', { err: true })
     } else {
         fs.readFile(db, (err, data) => {
@@ -38,7 +40,8 @@ app.post('/create', (req, res) => {
             posts.push({
                 id: id(),
                 title: title,
-                description: description,
+                snippet: snippet,
+                description: description
             })
 
             fs.writeFile(db, JSON.stringify(posts), err => {
@@ -54,7 +57,7 @@ app.post('/create', (req, res) => {
 
 
 app.get('/api/v1/posts', (req, res) => {
-    fs.readFile('./data/posts.json', (err, data) => {
+    fs.readFile(db, (err, data) => {
         if (err) throw err
 
         const posts = JSON.parse(data)
@@ -64,7 +67,7 @@ app.get('/api/v1/posts', (req, res) => {
 })
 
 app.get('/posts', (req, res) => {
-    fs.readFile('./data/posts.json', (err, data) => {
+    fs.readFile(db, (err, data) => {
         if (err) throw err
 
         const posts = JSON.parse(data)
@@ -76,7 +79,7 @@ app.get('/posts', (req, res) => {
 
 app.get('/posts/:id', (req, res) => {
     const id = req.params.id
-    fs.readFile('./data/posts.json', (err, data) => {
+    fs.readFile(db, (err, data) => {
         if (err) throw err
 
         const posts = JSON.parse(data)
@@ -102,8 +105,42 @@ app.get('/posts/:id/delete', (req, res) => {
         console.log(filteredPosts)
         fs.writeFile(db, JSON.stringify(filteredPosts), err => {
             if (err) throw err
-            res.render('posts', { id: id, posts: filteredPosts })
+
+
+            res.render('posts', { id: id, posts: filteredPosts, deleted: true })
         })
+    })
+})
+
+
+app.get('/posts/:id/archive', (req, res) => {
+    const id = req.params.id
+
+    fs.readFile(db, (err, data) => {
+        const posts = JSON.parse(data)
+
+        const post = posts.filter(post => post.id == id)[0]
+        const postId = posts.indexOf(post)
+        const splicedPost = posts.splice(postId, 1)[0]
+        splicedPost.archived = true
+        posts.push(splicedPost)
+
+        fs.writeFile(db, JSON.stringify(posts), (err) => {
+            if (err) throw err
+
+            res.redirect('/posts')
+        })
+
+    })
+})
+
+
+app.get('/archive', (req, res) => {
+    fs.readFile(db, (err, data) => {
+        if (err) throw err
+
+        const posts = JSON.parse(data).filter(post => post.archived == true)
+        res.render('posts', { title: "Hey", posts: posts });
     })
 })
 
